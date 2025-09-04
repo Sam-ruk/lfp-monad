62a8b37d6ec4880c23345194615d905791a49fe4
Sam-ruk <samk.freelance@gmail.com>
2025-09-04 14:40:07 +0530

    New maze

--- src/app/components/PFPs.tsx
+++ src/app/components/PFPs.tsx
@@ -422,7 +422,7 @@ const PFPs = () => {
                             }}
                           />
                           <div
-                            onClick={() => document.querySelector('input[type="color"]')?.click()}
+                            onClick={() => (document.querySelector('input[type="color"]') as HTMLInputElement)?.click()}
                             className="w-10 h-10 rounded-lg border-2 border-gray-200 cursor-pointer shadow-md"
                             style={{
                               background: backgroundColor,
